30a8ea2c752cbac0d5c8661b482caffbafc077b5
BreakTime94 <kimchan9408@gmail.com>
2025-12-11 21:04:00 +0900

    251211 프로젝트 생성 및 docker container db 연결 check 완료

---
+++ src/main/java/com/example/genie_tune_java/GenieTuneJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.genie_tune_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenieTuneJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GenieTuneJavaApplication.class, args);
+  }
+
+}
