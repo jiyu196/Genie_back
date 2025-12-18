3e6e8362b7e52ad9ef4e26d1f1c954029615a345
BreakTime94 <kimchan9408@gmail.com>
2025-12-18 16:10:15 +0900

    251218 사업자등록증 유효성 검증 로직 Junit Test 완료

--- src/main/java/com/example/genie_tune_java/api/nts/dto/validation/BusinessValidationInputData.java
+++ src/main/java/com/example/genie_tune_java/api/nts/dto/validation/BusinessValidationInputData.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record BusinessValidationInputData(
   @JsonProperty("b_no") String bizNumber,
-  @JsonProperty("start_dt") String openingDate,
   @JsonProperty("p_nm") String representativeName,
+  @JsonProperty("start_dt") String openingDate,
   @JsonProperty("p_nm2") String representativeName2,
   @JsonProperty("b_nm") String businessName,
   @JsonProperty("corp_no") String corpNumber,
